7854eb4f00f56bcfa886a48b02200e9e8bafbd33
thth13 <thth13q@gmail.com>
2019-02-23 19:13:28 +0200

    add modal

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,30 @@
 ﻿'use strict';
 
 $(document).ready(function() {
-    $(".link--dropdown").click(function() {
-      $(".link-categories").slideToggle();
-    });
+  $(".link--dropdown").click(function() {
+    $(".link-categories").slideToggle();
+  });
+
+  $("#login").click(function() {
+    $("#modal-login").toggle();
+  });
+
+  $("#register").click(function() {
+    $("#modal-login").toggle();
+    $("#modal-register").toggle();
+  });
+
+  $("#haveacc").click(function() {
+    $("#modal-login").toggle();
+    $("#modal-register").toggle();
+  });
+
+  $("#close1").click(function() {
+    $("#modal-login").toggle();;
+  });
+
+  $("#close2").click(function() {
+    $("#modal-register").toggle();
+  });
+
 });
