ba8567e93c2d2dc38282b61665a77978b9aba2f3
Sasha <sashapayareli@gmail.com>
2019-02-23 19:25:42 +0200

    comments js

--- source/js/script.js
+++ source/js/script.js
@@ -5,3 +5,24 @@ $(document).ready(function() {
       $(".link-categories").slideToggle();
     });
 });
+
+//Комменты
+
+let comments = document.querySelectorAll('.comment');
+
+for (let i = 0; i < comments.length; i++) {
+  let elem = comments[i];
+  let parentEl = elem.parentNode;
+
+  if (parentEl.classList.contains('comment')) { // Если коммент вложен в другой коммент
+
+    let wrapperEl = elem.firstElementChild;
+
+   wrapperEl.style.marginTop = '0px';
+   wrapperEl.style.marginLeft = '40px';
+
+    if (parentEl.parentNode.classList.contains('comment')) {// И там еще выше есть коммент  
+      wrapperEl.style.marginLeft = '80px'; //3-ий уровень вложености 
+    }
+  }
+}
