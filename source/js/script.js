06b59ff31fa14551f0ee20fefa1e42b03ad13b43
Sasha <sashapayareli@gmail.com>
2019-02-23 19:45:21 +0200

    fixes

--- source/js/script.js
+++ source/js/script.js
@@ -1,33 +1,6 @@
 ﻿'use strict';
 
 $(document).ready(function() {
-<<<<<<< HEAD
-    $(".link--dropdown").click(function() {
-      $(".link-categories").slideToggle();
-    });
-});
-
-//Комменты
-
-let comments = document.querySelectorAll('.comment');
-
-for (let i = 0; i < comments.length; i++) {
-  let elem = comments[i];
-  let parentEl = elem.parentNode;
-
-  if (parentEl.classList.contains('comment')) { // Если коммент вложен в другой коммент
-
-    let wrapperEl = elem.firstElementChild;
-
-   wrapperEl.style.marginTop = '0px';
-   wrapperEl.style.marginLeft = '40px';
-
-    if (parentEl.parentNode.classList.contains('comment')) {// И там еще выше есть коммент  
-      wrapperEl.style.marginLeft = '80px'; //3-ий уровень вложености 
-    }
-  }
-}
-=======
   $(".link--dropdown").click(function() {
     $(".link-categories").slideToggle();
   });
@@ -55,4 +28,24 @@ for (let i = 0; i < comments.length; i++) {
   });
 
 });
->>>>>>> 7854eb4f00f56bcfa886a48b02200e9e8bafbd33
+
+//Комменты
+
+let comments = document.querySelectorAll('.comment');
+
+for (let i = 0; i < comments.length; i++) {
+  let elem = comments[i];
+  let parentEl = elem.parentNode;
+
+  if (parentEl.classList.contains('comment')) { // Если коммент вложен в другой коммент
+
+    let wrapperEl = elem.firstElementChild;
+
+   wrapperEl.style.marginTop = '0px';
+   wrapperEl.style.marginLeft = '40px';
+
+    if (parentEl.parentNode.classList.contains('comment')) {// И там еще выше есть коммент  
+      wrapperEl.style.marginLeft = '80px'; //3-ий уровень вложености 
+    }
+  }
+}
