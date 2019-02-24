8b5c9671051d04e27c808c3d36d75c9157f0b2b2
thth13 <thth13q@gmail.com>
2019-02-24 09:17:56 +0200

    upd

---
+++ source/js/script2.js
@@ -0,0 +1,44 @@
+axios.get('http://hackathon.xx.org.ua/api/news?sort_field=name&sort_type=desc&limit=8')
+  .then((res) => {
+    res.data.map(item => {
+      console.log(item)
+      $('.posts__list').append(`
+        <li class="news posts__items">
+        <div class="news__image">
+          <img src="${item.image}" alt="Header">
+        </div>
+        <div class="news__wrapper">
+          <h3 class="news__header">
+            <a href="/post.html">${item.name}</a>
+          </h3>
+          <p class="news__description">${item.content}</p>
+          <a class="news__more" href="#">read more..</a>
+        </div>
+      </li>
+      `);
+    })
+  })
+  .catch((err) => console.log('err'))
+
+  axios.get('http://hackathon.xx.org.ua/api/news/get/main')
+    .then(res => {
+        $('.posts__list').prepend(`
+        <li class="news news--main posts__items">
+        <div class="news__image">
+          <img src="img/600x400.jpg" alt="Header">
+        </div>
+        <div class="news__wrapper">
+          <h3 class="news__header">
+            <a href="#">${res.data[0].name}</a>
+          </h3>
+          <p class="news__description">${res.data[0].content}</p>
+        </div>
+      </li>
+      `);
+    })
+
+// function loadPost(id) {
+//  axios.get('http://hackathon.xx.org.ua/api/news/get/main')
+//     .then(res => console.log(res))
+
+// }
